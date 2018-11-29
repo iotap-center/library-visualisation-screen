68087cd456ec211faac0d76a908225dba3ab9065
Johan Holmberg <johan@idioti.se>
2018-11-29 13:02:55 +0100

    Added a creepy waiting screen

--- public/main.js
+++ public/main.js
@@ -1,38 +1,49 @@
+var gracePeriod = 30 * 1000;
+
 setInterval(function () {
     // Function that calls a GET-request every 5th second to fetch user data
     $.get('http://iotap-library-visualisation-screen.eu-west-1.elasticbeanstalk.com/data', function(data) {
       // Loads in data of users total time spent at different beacons
       // and pushes the data to graph visualizer
-      var cube = data.visits.cube;
-      var diss = data.visits.diss1 + data.visits.diss2 + data.visits.diss3;
-      var ecos = data.visits.ecos1 + data.visits.ecos2 + data.visits.ecos3;
-      var wtw = data.visits.wtw1 + data.visits.wtw2 + data.visits.wtw3 + data.visits.wtw4 + data.visits.wtw5;
-      var values = [cube, diss, ecos, wtw];
-      Plotly.restyle('my-graph', 'values', [[values]]);
-  
-  
-      // Loads in users answers and shows a fake ad according to answers given
-      var answers = data.answers;
-      // AI Legal (set picture)
-      if (answers.wtw1 == "no" && answers.wtw3 == "yes" && answers.wtw5 == "correct") {
-        $("img").attr("src", "../public/images/ailegal.png");
-      } 
-      // Smart Security (set picture)
-      else if (answers.wtw3 == "no") {
-        $("img").attr("src", "../public/images/smartsecurity.png");
+      var date = new Date(data.uploaded);
+      if (Date.now() - date > gracePeriod) {
+    	  // Show waiting screen
+    	  $("#waiting-screen").show();
+          $("#graph-screen").hide();
+      } else {
+    	  // Show data
+    	  var cube = data.visits.cube;
+          var diss = data.visits.diss1 + data.visits.diss2 + data.visits.diss3;
+          var ecos = data.visits.ecos1 + data.visits.ecos2;
+          var wtw = data.visits.wtw1 + data.visits.wtw2 + data.visits.wtw3 + data.visits.wtw4 + data.visits.wtw5;
+          var values = [cube, diss, ecos, wtw];
+          Plotly.restyle('my-graph', 'values', [values]);
+      
+      
+          // Loads in users answers and shows a fake ad according to answers given
+          var answers = data.answers;
+          // AI Legal (set picture)
+          if (answers.wtw1 == "no" && answers.wtw3 == "yes" && answers.wtw5 == "correct") {
+            $("#banner").attr("src", "/images/ailegal.png");
+          } 
+          // Smart Security (set picture)
+          else if (answers.wtw3 == "no") {
+            $("#banner").attr("src", "/images/smartsecurity.png");
+          }
+          // Surrogate (set picture)
+          else if (answers.wtw1 == "yes" && answers.wtw5 == "correct") {
+            $("#banner").attr("src", "/images/surrogate.png");
+          }
+          // Vitacrispr (set picture)
+          else if (answers.wtw3 == "dontKnow" && answers.wtw4 == "wrong" && answers.wtw5 == "wrong") {
+            $("#banner").attr("src", "/images/vitacrispr.png");
+          }
+          else {
+            $("#banner").attr("src", "/images/surrogate.png");
+          }
+          $("#waiting-screen").hide();
+          $("#graph-screen").show();
       }
-      // Surrogate (set picture)
-      else if (answers.wtw1 == "yes" && answers.wtw5 == "correct") {
-        $("img").attr("src", "../public/images/surrogate.png");
-      }
-      // Vitacrispr (set picture)
-      else if (answers.wtw3 == "dontKnow" && answers.wtw4 == "wrong" && answers.wtw5 == "wrong") {
-        $("img").attr("src", "../public/images/vitacrispr.png");
-      }
-      else {
-        $("img").attr("src", "../public/images/surrogate.png");
-      }
-  
     });
   }, 5000);
   
